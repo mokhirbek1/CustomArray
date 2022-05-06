f4fcb3dd324d8d225f895cdbbef695a76c87e262
moxirbek021199 <moxirbek021199@gmail.com>
2022-05-06 05:40:03 +0500

    Today

--- test/java/epam/learning/task/action/ActionTest.java
+++ test/java/epam/learning/task/action/ActionTest.java
@@ -1,10 +1,20 @@
 package epam.learning.task.action;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class ActionTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
 
     @Test
     public void testCreateCustomArray() {
