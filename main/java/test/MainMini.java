5f29394083e78766a619cc82e1b2788cda65a320
Moxirbek <moxirbek021199@gmail.com>
2023-08-17 05:07:45 +0500

    Ready for exam

--- main/java/test/MainMini.java
+++ main/java/test/MainMini.java
@@ -3,5 +3,17 @@ package test;
 public class MainMini {
     public static void main(String[] args) {
         System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
+        System.out.println("Ready for exam!");
     }
 }
